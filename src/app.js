0130ecea691d2bbc9dc38fcbcc210d608232a5fd
suyashthonte0 <suyashthonte0@gmail.com>
2020-02-25 18:03:51 +0530

    project-12-superwars-basic

--- src/app.js
+++ src/app.js
@@ -21,12 +21,25 @@ const PLAYERS = [
     "Slingo"
 ];
 
+
 // initialize players with image and strength
 const initPlayers = (players) => {
     let detailedPlayers = [];
+    detailedPlayers.length = 16;
+    for(var i = 0; i < PLAYERS.length; i++)
+    {
+        var players = {
+            name : PLAYERS[i],
+            strength : getRandomStrength(),
+            image : src = "../project-12-superwars-basic/src/images/super-"+(i+1)+".png",
+            type : "hero|villain"
+        }
+        detailedPlayers.push(players);
+        document.write(detailedPlayers);
+    }
     // Create players using for loop
     // Type your code here
-
+    player = i % 2 == 0 ? 'true' : 'false';
     return detailedPlayers;
 }
 
@@ -34,11 +47,21 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    let random_strength = Math.floor(Math.random()*100)+1;
+    return random_strength;
 }
 
 const buildPlayers = (players, type) => {
-    let fragment = '';
-
+    let fragment = ``;
+    for(var i = 0;i < PLAYERS.length;i++)
+    {
+        fragment = `
+        <div class="player">
+        <img src="${detailedPlayers[i].image}" alt = "superheroes">
+        <h3> ${detailedPlayers[i].name} </h3>
+        <h1> ${detailedPlayers[i].strength} </h1>
+        `;
+    }
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
