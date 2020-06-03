47405de5cf9882d75304c7a868ddbe5e5a1199cf
VasaviKashyap123 <65177128+VasaviKashyap123@users.noreply.github.com>
2020-06-03 02:24:17 -0700

    modif

--- src/app.js
+++ src/app.js
@@ -22,37 +22,48 @@ const PLAYERS = [
 ];
 
 // initialize players with image and strength
-const initPlayers = (players) => {
-    let detailedPlayers = [];
+const initPlayers = players => {
+    var detailedPlayers = [];
     // Create players using for loop
     // Type your code here
+    let position = 0,
+        size = players.length;
+    for (position, size; size > 0; size--, position++) {
+        detailedPlayers[position] = {
+            name: players[position],
+            strength: getRandomStrength(),
+            image: "images/super-" + (position + 1) + ".png"
+        };
 
+        if ((position) % 2 == 0) {
+            detailedPlayers[position].type = "hero";
+        } else {
+            detailedPlayers[position].type = "villan";
+        }
+    }
     return detailedPlayers;
-}
+};
 
 // getting random strength
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
-}
+    return Math.random() * 100;
+};
 
 const buildPlayers = (players, type) => {
     let fragment = '';
 
-    // Loop through players and accumulate HTML template
-    // depending of type of player(hero|villain)
-    // Type your code here
+    for (i = 0; i < players.length; i++) {
+        player = `
+          <div class="player">
+              <img src="${players[i].image}" alt="">
+              <div class="name">${players[i].name}</div>
+              <div class="strength">${players[i].strength}</div>
+          </div>`;
+        if (players[i].type == type)
+            fragment = fragment + player;
+    }
 
     return fragment;
-}
-// Display players in HTML
-const viewPlayers = (players) => {
-
-    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
-    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
-
-}
-
-window.onload = () => {
-    viewPlayers(initPlayers(PLAYERS));
 }
