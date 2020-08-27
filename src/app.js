d55ce239661e4f2cf85bb4a6f1e0771c4c4b5107
abhid07 <a.deshpande4509@gmail.com>
2020-08-28 01:39:32 +0530

    FSJ0820B104

--- src/app.js
+++ src/app.js
@@ -26,23 +26,53 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
+    for(i=0;i<players.length;i++)
+    {
+        detailedPlayers = [{
+            name: players[i],
+            strength: 100,
+            image: "images/super-1.png",
+            type: (i % 2 == 0) ? 'hero' : 'villain'
+        }]
+    }
 
     return detailedPlayers;
 }
+
+
 
 // getting random strength
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    return(Math.random(0,100))
 }
 
+//### Progression 3: No player should fall
 const buildPlayers = (players, type) => {
-    let fragment = '';
+
 
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
+    let fragment = '';
 
+    for (let i of players) {
+
+        if(i.type==type)
+        {
+            fragment = `
+            <div class="player">
+                <img src="${i.image}" alt="">
+                <div class="name">${i.name}</div>
+                <div class="strength">${i.strength}</div>
+            </div>`;
+        }
+
+        // Loop through players and accumulate HTML template
+        // depending of type of player(hero|villain)
+        // Type your code here
+    }
     return fragment;
 }
 // Display players in HTML
