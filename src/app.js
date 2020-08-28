ad6c3da0ef57354065c5462886517499880a3ca6
Ranjith JS <ranjithjs1998.rj@gmail.com>
2020-08-28 14:24:33 +0530

    FSJ0820B101

--- src/app.js
+++ src/app.js
@@ -24,24 +24,38 @@ const PLAYERS = [
 // initialize players with image and strength
 const initPlayers = (players) => {
     let detailedPlayers = [];
-    // Create players using for loop
-    // Type your code here
-
+    for (let i = 0; i < players.length; i++) {
+        detailedPlayers[i] = {
+            name: players[i],
+            image: "images/super-" + (i + 1) + ".png",
+            strength: getRandomStrength(),
+            type: i % 2 == 0 ? "hero" : "villain",
+        };
+    }
     return detailedPlayers;
 }
 
+
+
 // getting random strength
 const getRandomStrength = () => {
-    // Return a random integer (0,100]
-    // Note: You can use Math.random() and Math.ceil()
+
+    return Math.ceil(Math.random() * 100);
 }
 
 const buildPlayers = (players, type) => {
     let fragment = '';
-
-    // Loop through players and accumulate HTML template
-    // depending of type of player(hero|villain)
-    // Type your code here
+    for (let i = 0; i < players.length; i++) {
+        player =
+            `<div class="player">
+         <img src="${players[i].image}" alt="" > 
+         <div class="name">${players[i].name}</div>
+         <div class="strength">${players[i].strength}</div>
+      </div>`
+        if (players[i].type == type) {
+            fragment = fragment + player;
+        }
+    }
 
     return fragment;
 }
