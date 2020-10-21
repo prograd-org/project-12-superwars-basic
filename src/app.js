98bdbf0fc5c1adbfdd3668f1fd1280b153695a78
abhid07 <a.deshpande4509@gmail.com>
2020-10-21 13:00:16 +0530

    FSJ0820B104

--- src/app.js
+++ src/app.js
@@ -26,14 +26,14 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
-    for(i=0;i<players.length;i++)
+    for(let i=0;i<players.length;i++)
     {
-        detailedPlayers = [{
+        detailedPlayers[i] = {
             name: players[i],
-            strength: 100,
-            image: "images/super-1.png",
+            strength: getRandomStrength(),
+            image: "images/super-"+(i+1)+".png",
             type: (i % 2 == 0) ? 'hero' : 'villain'
-        }]
+        }
     }
 
     return detailedPlayers;
@@ -45,7 +45,8 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
-    return(Math.random(0,100))
+    var strength = Math.random()*100
+    return (Math.ceil(strength))
 }
 
 //### Progression 3: No player should fall
@@ -57,22 +58,21 @@ const buildPlayers = (players, type) => {
     // Type your code here
     let fragment = '';
 
-    for (let i of players) {
-
-        if(i.type==type)
-        {
-            fragment = `
+    for(let i=0;i<players.length;i++)
+    {
+       
+            player= `
             <div class="player">
-                <img src="${i.image}" alt="">
-                <div class="name">${i.name}</div>
-                <div class="strength">${i.strength}</div>
+                <img src="${players[i].image}" alt="">
+                <div class="name">${players[i].name}</div>
+                <div class="strength">${players[i].strength}</div>
             </div>`;
+        if (players[i].type == type) {
+            fragment+=player
         }
-
-        // Loop through players and accumulate HTML template
-        // depending of type of player(hero|villain)
-        // Type your code here
     }
+
+
     return fragment;
 }
 // Display players in HTML
